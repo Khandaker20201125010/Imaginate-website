c258757c7eb00741fd64693cf64ba31e1fdb5920
pranto <prantokih42@gmail.com>
2024-07-28 14:21:43 +0600

    homepage designed

--- src/Components/HomeBody/HomeBody.jsx
+++ src/Components/HomeBody/HomeBody.jsx
@@ -1,19 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import BodyParticle from '../BodyParticle/BodyParticle';
-import { initHackerEffect } from '../hackereff/hackereff';
 
 const HomeBody = () => {
-    useEffect(() => {
-        initHackerEffect(); // Initialize the effect when the component mounts
-      }, []);
-    
-    return (
-        <div className='flex items-center justify-center min-h-screen'>
-            <h1 className='animated-text text-center p-20 m-auto ho text-8xl ' data-value="Welcome to the Imaginate">Welcome to the Imaginate </h1>
-            <BodyParticle></BodyParticle>
-            
-        </div>
-    );
+  return (
+    <div className="relative w-full h-screen overflow-hidden animated-background">
+      <BodyParticle />
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
+        <h1 className="text-4xl font-bold animated-text">Welcome to the Gaming World</h1>
+        <p className="mt-5 text-lg">Prepare for an epic adventure</p>
+      </div>
+    </div>
+  );
 };
 
 export default HomeBody;
